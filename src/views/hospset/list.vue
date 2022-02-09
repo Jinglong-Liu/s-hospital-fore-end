b4d153dc0c8508ab8fdfe47d8f5063002807e903
ljl <1601029287@qq.com>
2022-02-09 18:01:30 +0800

    List in browse. Basic frame has been created!!!

--- src/views/hospset/list.vue
+++ src/views/hospset/list.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="app-container">
     医院设置列表
+    <el-table
+      :data="list"
+      style="width: 100%"
+    >
+      <el-table-column type="index" width="50" label="序号" />
+      <el-table-column prop="hosname" label="医院名称" />
+      <el-table-column prop="hoscode" label="医院编号" />
+      <el-table-column prop="apiUrl" label="api基础路径" width="200" />
+      <el-table-column prop="contactsName" label="联系人姓名" />
+      <el-table-column prop="contactsPhone" label="联系人手机" />
+      <el-table-column label="状态" width="80">
+        <template slot-scope="scope">
+          {{ scope.row.status === 1 ? '可用' : '不可用' }}
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
@@ -12,7 +28,8 @@ export default {
       current: 1, // 当前页
       limit: 3, // 每页
       searchObj: {}, // 条件封装对象
-      list: [] // 每页数据集合
+      list: [], // 每页数据集合
+      total: 0 // 总记录数
     }
   },
   created() {
@@ -24,6 +41,10 @@ export default {
     getList() {
       hospset.getHospSetList(this.current, this.limit, this.searchObj)
         .then(response => {
+          // 返回集合赋值给list
+          this.list = response.data.records
+          // 总数
+          this.total = response.data.total
           console.log(response)
         })
         .catch(error => {
