d326103ed7e88a4d3eec79a664dc8e32bd14e488
ljl <1601029287@qq.com>
2022-02-10 10:42:52 +0800

    import dict

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -5,7 +5,7 @@
         <a href="http://localhost:8202/admin/cmn/dict/exportData" target="_blank">
           <el-button type="text"><i class="fa fa-plus" /> 导出</el-button>
         </a>
-        <!--el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button-->
+        <el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button>
       </div>
     </div>
     <el-table
@@ -39,6 +39,26 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+      <el-form label-position="right" label-width="170px">
+        <el-form-item label="文件">
+          <el-upload
+            :multiple="false"
+            :on-success="onUploadSuccess"
+            :action="'http://localhost:8202/admin/cmn/dict/importData'"
+            class="upload-demo">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传excel文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">
+          取消
+        </el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -47,7 +67,8 @@ import dict from '@/api/dict'
 export default {
   data() {
     return {
-      list: []
+      list: [],
+      dialogImportVisible: false // 弹框是否弹出
     }
   },
   created() {
@@ -71,6 +92,17 @@ export default {
     exportData() {
       // 调用导出接口
       window.location.href = 'http://localhost:8202/admin/cmn/dict/exportData'
+    },
+    // 导入数据字典
+    importData() {
+      this.dialogImportVisible = true
+    },
+    // 上传成功调用
+    onUploadSuccess() {
+      // 关闭弹框
+      this.dialogImportVisible = false
+      // 刷新页面
+      this.getDictList(1)
     }
   }
 }
