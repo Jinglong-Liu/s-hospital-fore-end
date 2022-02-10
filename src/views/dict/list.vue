075d66d260122558dadb33e84c82d6d3ffaa289a
ljl <1601029287@qq.com>
2022-02-10 10:08:30 +0800

    export excel

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="app-container">
+    <div class="el-toolbar">
+      <div class="el-toolbar-body" style="justify-content: flex-start;">
+        <a href="http://localhost:8202/admin/cmn/dict/exportData" target="_blank">
+          <el-button type="text"><i class="fa fa-plus" /> 导出</el-button>
+        </a>
+        <!--el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button-->
+      </div>
+    </div>
     <el-table
       :data="list"
       style="width: 100%"
@@ -58,6 +66,11 @@ export default {
       dict.dictList(tree.id).then(response => {
         resolve(response.data)
       })
+    },
+    // 导出数据字典
+    exportData() {
+      // 调用导出接口
+      window.location.href = 'http://localhost:8202/admin/cmn/dict/exportData'
     }
   }
 }
